51bff6288735d63a060c1a3cabd5cb1691acb229
OllyC1 <you@example.com>
2025-06-17 22:45:51 +0100

    Remove legacy project files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Competition-Management-web-portal
-Developed a comprehensive web portal for a national cycling competition, allowing users to register for events and administrators to manage participant data
--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
- //database connection variables for your UOS webspace database
- $servername = "localhost";
- $username = "Cyclingcompt1";
- $password = "Olayemi_2003";
- $database = "Cyclingcompetition"; 
-
- ?>
